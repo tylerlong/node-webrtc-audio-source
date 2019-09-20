dc4657dbcab139508e5806e54f537a28eebcb127
Tyler Long <tyler4long@gmail.com>
2019-09-20 16:16:07 -0700

    Update buffer size

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ class NodeWebRtcAudioSource extends RTCAudioSource {
 
   start () {
     this.stop()
-    this.ps = spawn('rec', ['-q', '-b', 16, '-r', 48000, '-e', 'signed', '-c', 1, '-t', 'raw', '-'])
+    this.ps = spawn('rec', ['-q', '-b', 16, '-r', 48000, '-e', 'signed', '-c', 1, '-t', 'raw', '--buffer', 960, '-'])
     this.ps.stdout.on('data', buffer => {
       const samples = new Int16Array(new Uint8Array(buffer).buffer)
       this.data({
