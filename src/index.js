596a5b950e9fd43bdd8399a7c5d5ccb81b1368a4
Tyler Long <tyler4long@gmail.com>
2019-09-20 16:28:36 -0700

    Fix a typo

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ class NodeWebRtcAudioSource extends RTCAudioSource {
     this.ps = spawn('rec', ['-q', '-b', 16, '-r', 48000, '-e', 'signed', '-c', 1, '-t', 'raw', '--buffer', 960, '-'])
     this.ps.stdout.on('data', buffer => {
       const samples = new Int16Array(new Uint8Array(buffer).buffer)
-      this.data({
+      this.onData({
         bitsPerSample: 16,
         sampleRate: 48000,
         channelCount: 1,
